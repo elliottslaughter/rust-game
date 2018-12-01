0347961346debcde214f094fc5327b9a8bbb29dd
Elliott Slaughter <elliottslaughter@gmail.com>
2018-12-01 13:58:31 -0800

    Reformat.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
 use std::time::Duration;
 
 use game::error::Error;
@@ -9,12 +9,13 @@ fn main() -> Result<(), Error> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+    let window = video_subsystem
+        .window("demo", 800, 600)
         .position_centered()
         .build()?;
 
     let mut canvas = window.into_canvas().build()?;
-    
+
     canvas.set_draw_color(Color::RGB(0, 255, 255));
     canvas.clear();
     canvas.present();
@@ -26,16 +27,17 @@ fn main() -> Result<(), Error> {
         canvas.clear();
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
                 _ => {}
             }
         }
 
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-    };
+    }
     Ok(())
 }
