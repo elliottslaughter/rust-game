c764606d89bed54e5d80f57a714c56be6778d1d2
Elliott Slaughter <elliottslaughter@gmail.com>
2018-12-02 15:07:44 -0800

    Tweak facing and animation.

--- src/control.rs
+++ src/control.rs
@@ -13,6 +13,7 @@ pub struct Control {
     down_pressed: bool,
     left_pressed: bool,
     right_pressed: bool,
+    space_pressed: bool,
 
     // Track the current control state for opposing direction keys.
     pub up_down_input: i32,    // -1/0/+1 for up/no input/down
@@ -29,6 +30,8 @@ pub struct Control {
 }
 
 pub fn process_input(event_pump: &mut EventPump, control: &mut Control) -> Result<(), Error> {
+    control.attack_input = false; // Attack only on initial keypress.
+
     for event in event_pump.poll_iter() {
         match event {
             Event::Quit { .. } => {
@@ -44,41 +47,34 @@ pub fn process_input(event_pump: &mut EventPump, control: &mut Control) -> Resul
                 Keycode::Up => {
                     if !control.up_pressed {
                         control.up_down_input = -1;
-                        if control.left_right_input == 0 {
-                            control.facing_input = 0;
-                        }
+                        control.facing_input = 0;
                     }
                     control.up_pressed = true;
                 }
                 Keycode::Down => {
                     if !control.down_pressed {
                         control.up_down_input = 1;
-                        if control.left_right_input == 0 {
-                            control.facing_input = 2;
-                        }
+                        control.facing_input = 2;
                     }
                     control.down_pressed = true;
                 }
                 Keycode::Left => {
                     if !control.left_pressed {
                         control.left_right_input = -1;
-                        if control.up_down_input == 0 {
-                            control.facing_input = 1;
-                        }
+                        control.facing_input = 1;
                     }
                     control.left_pressed = true;
                 }
                 Keycode::Right => {
                     if !control.right_pressed {
                         control.left_right_input = 1;
-                        if control.up_down_input == 0 {
-                            control.facing_input = 3;
-                        }
+                        control.facing_input = 3;
                     }
                     control.right_pressed = true;
                 }
                 Keycode::Space => {
-                    control.attack_input = true;
+                    control.attack_input = !control.space_pressed;
+                    control.space_pressed = true;
                 }
                 _ => {}
             },
@@ -90,28 +86,32 @@ pub fn process_input(event_pump: &mut EventPump, control: &mut Control) -> Resul
                     control.up_pressed = false;
                     if control.up_down_input == -1 {
                         control.up_down_input = 0;
+                        control.facing_input = control.left_right_input + 2;
                     }
                 }
                 Keycode::Down => {
                     control.down_pressed = false;
                     if control.up_down_input == 1 {
                         control.up_down_input = 0;
+                        control.facing_input = control.left_right_input + 2;
                     }
                 }
                 Keycode::Left => {
                     control.left_pressed = false;
                     if control.left_right_input == -1 {
                         control.left_right_input = 0;
+                        control.facing_input = control.up_down_input + 1;
                     }
                 }
                 Keycode::Right => {
                     control.right_pressed = false;
                     if control.left_right_input == 1 {
                         control.left_right_input = 0;
+                        control.facing_input = control.up_down_input + 1;
                     }
                 }
                 Keycode::Space => {
-                    control.attack_input = false;
+                    control.space_pressed = false;
                 }
                 _ => {}
             },
